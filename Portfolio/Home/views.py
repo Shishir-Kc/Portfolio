8508349039788dfc1074490b053688f455655179
Shishir_Kc <shishir.khatri.kc@gmail.com>
2025-07-29 21:12:16 +0545

    added message handeling !

--- Portfolio/Home/views.py
+++ Portfolio/Home/views.py
@@ -1,8 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render , redirect
 from django.http import HttpResponse
-
+from . import models
+from django.contrib import messages
 def Home(request):
     return render (request,"block/index.html")
 
 def protfolio(request):
     return render (request,"protfolio/protfolio.html")
+
+
+def contact(request):
+    if request.method == "POST":
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        message = request.POST.get('message')
+        data = models.Contact.objects.create(name=name,email=email,message=message)
+        data.save()
+        messages.success(request,"Recived your Interest ! ,")
+        return redirect('home:protfolio')
+        
