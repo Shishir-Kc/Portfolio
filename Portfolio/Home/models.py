576e5590c6fb4d129582c8a79e6f8d38ad607fb9
Shishir_Kc <shishir.khatri.kc@gmail.com>
2025-07-29 21:13:38 +0545

    added message db

--- Portfolio/Home/models.py
+++ Portfolio/Home/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Contact(models.Model):
+    name = models.CharField(verbose_name='user name')
+    email = models.EmailField(verbose_name='user_mail')
+    message = models.TextField(verbose_name='user_message!')
+
+    class Meta:
+        verbose_name = "contact"
+
+    def __str__(self):
+        return self.name
